2094201e24db71ab07a21ff202301d6c47a8a4e5
Luke Mondy <luke.s.mondy@gmail.com>
2016-09-06 15:36:42 +1000

    Removed settings from git, so it can be secret

--- askfm_mvc/settings.py
+++ askfm_mvc/settings.py
@@ -4,3 +4,6 @@ DEBUG = True
 SECRET_KEY = 'the most secret key'
 SECURITY_PASSWORD_HASH = 'bcrypt'
 SECURITY_PASSWORD_SALT = SECRET_KEY
+
+ADMIN_EMAIL = 'luke.s.mondy@gmail.com'
+ADMIN_PASSWORD = 'password'
