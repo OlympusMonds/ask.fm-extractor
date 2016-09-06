9d567189991521e054e6b205c470809cb6631639
Luke Mondy <luke.s.mondy@gmail.com>
2016-09-06 15:38:18 +1000

    Actually delete it now

--- askfm_mvc/settings.py
+++
@@ -1,9 +0,0 @@
-DATABASE = 'sqlite://'
-DEBUG = True
-
-SECRET_KEY = 'the most secret key'
-SECURITY_PASSWORD_HASH = 'bcrypt'
-SECURITY_PASSWORD_SALT = SECRET_KEY
-
-ADMIN_EMAIL = 'luke.s.mondy@gmail.com'
-ADMIN_PASSWORD = 'password'
