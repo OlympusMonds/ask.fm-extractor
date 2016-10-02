663e7c1a2e293f5b04f9ab51466b726410f2c032
Luke Mondy <luke.s.mondy@gmail.com>
2016-10-02 22:12:04 +1100

    Many upgrades in how data is extracted from ask

--- askfm_mvc/extract.py
+++ askfm_mvc/extract.py
@@ -1,13 +1,13 @@
 
 from bs4 import BeautifulSoup
-import urllib
-import cPickle as pickle
+from urllib import request
+import pickle
 import os
 import argparse
 
 
 def get_html(url):
-    r = urllib.urlopen(url)
+    r = request.urlopen(url)
     return r.read()
 
 
@@ -16,18 +16,46 @@ def read_html(doc):
     return soup
 
 
+def get_image_as_str(url):
+    r = request.urlopen(url)
+    return r.read()
+
+
 def process_soup(sp, newline):
     whole_answer_chunks = sp.find_all("div", class_="item streamItem streamItem-answer")
 
     whole_answer_list = []
 
     for ans in whole_answer_chunks:
-        ansdict = {"question" : "",
-                   "answer"   : ""}
-        ansdict["question"] = ans.find("h1", class_="streamItemContent streamItemContent-question").get_text().strip()
+        qanda = {"id"       : -1,
+                 "question" : "",
+                 "answer"   : "",
+                 "asker"    : "Anon",
+                 "date"     : "",
+                 "url"      : "",
+                 "likes"    : 0,
+                 "media"    : None,}
+
+        question = ans.find("div", class_="streamItemContent streamItemContent-question")
+        qanda["question"] = question.find("h2").get_text().strip()
+        asker = question.find("a", class_="questionersName")
+        if asker:
+            qanda["asker"] = asker.get_text().strip()
+
         for anschunk in ans.find_all("p", class_="streamItemContent streamItemContent-answer"):
-            ansdict["answer"] += newline + anschunk.get_text()
-        whole_answer_list.append(ansdict)
+            qanda["answer"] += newline + anschunk.get_text()
+
+        meta = ans.find("a", class_="streamItemsAge")
+        qanda["date"] = meta["title"]
+        qanda["url"] = meta["href"]
+        qanda["id"] = int(qanda["url"].split("/")[-1])
+        qanda["likes"] = int(ans.find("a", class_="counter").get_text())
+
+        media = ans.find("div", class_="streamItem-visualItem")
+        if media:
+            qanda["media"] = get_image_as_str(media.find("a")["data-url"])
+
+        whole_answer_list.append(qanda)
 
     return whole_answer_list
 
@@ -51,23 +79,28 @@ def extract(account_name, newline = "\n", cache_raw=False):
         all_qa.extend(process_soup(sp, newline))
         pagenum += 1
         print("  Finished page num {}".format(pagenum))
+        if pagenum == 2:
+            break
         if len(all_qa) == 0:
             print("  Problem with data:\n{}".format(doc))
             break
-    
-    if cache_raw:
-        with open(picklefile, 'wb') as f:
-            pickle.dump( all_qa, f )
     
     output = []
     for qa in reversed(all_qa):
         try:
             output.append(newline)
-            output.append("=================")
-            output.append(qa["question"])
-            output.append("----")
+            output.append("{asker} asks: {question}".format(**qa))
+            output.append(newline)
             output.append(qa["answer"])
             output.append(newline)
+            output.append(qa["date"] + "  likes: " + str(qa["likes"]))
+            output.append(newline)
+            output.append(qa["url"])
+            output.append(newline)
+            if qa["media"]:
+                with open("{}.jpg".format(qa["id"]), "wb") as f:
+                    f.write(qa["media"])
+            #done
         except KeyError:
             output.append(newline + "ERROR OCCURRED" + newline)
 
@@ -80,4 +113,4 @@ if __name__ == "__main__":
     parser.add_argument("account name", type=str,
                         help="The person's username.")
     args = vars(parser.parse_args())
-    print extract(args["account name"])
+    print(extract(args["account name"]))
